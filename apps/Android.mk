c7150385d268b52bfb65a6fb51068cf3ee8a249d
phhgsi <ajay24ce008@satiengg.in>
2024-02-13 20:30:39 +0530

    remove gallery or add poweramp music app

--- apps/Android.mk
+++ apps/Android.mk
@@ -1,23 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := Gramophone
+LOCAL_MODULE := Poweramp
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := gramophone/Gramophone.apk
+LOCAL_SRC_FILES := poweramp/poweramp.apk
 LOCAL_PRODUCT_MODULE := true
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := QPGallery
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_TAGS := optional
-LOCAL_REPLACE_PREBUILT_APK_INSTALLED := $(LOCAL_PATH)/qpgallery/qpgallery.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := qpgallery/qpgallery.apk
-LOCAL_PRODUCT_MODULE := true
-LOCAL_OPTIONAL_USES_LIBRARIES := com.sec.android.app.multiwindow
-LOCAL_OVERRIDES_PACKAGES := Gallery Gallery2 SnapdragonGallery
 include $(BUILD_PREBUILT)
